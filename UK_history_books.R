63f9ebbbea416a13fe8b23638e5ef1ecc8fe36a3
lddurbin <l.d.durbin@gmail.com>
2020-09-15 19:42:06 +1200

    Filter out from the tweets any books that don't include "history"

--- UK_history_books.R
+++ UK_history_books.R
@@ -16,7 +16,7 @@ if(length(new_zip_files) > 0) {
 history_books <- read_csv("processed data/history_books.csv", col_types = "cccccccclcclDccc")
 
 tweet_data <- history_books %>%
-  filter(to_tweet == TRUE) %>% 
+  filter(to_tweet == TRUE & str_detect(str_to_lower(topic), "history") == TRUE) %>% 
   mutate(sample_id = group_indices(.,isbn)) %>%
   subset(sample_id %in% sample(unique(.$sample_id), 1))
 
